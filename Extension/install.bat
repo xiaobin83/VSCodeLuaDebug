77e074301ca6f418c1546ce2e21b0b26825b70b5
Seungjae Lee @home <distynova@gmail.com>
2016-12-27 19:01:23 +0900

    permission test

--- Extension/install.bat
+++ Extension/install.bat
@@ -1,2 +1,2 @@
-copy /y *.dll c:\users\Seungjae\.vscode\extensions\devCAT.lua-debug-0.1.1
-copy /y *.exe c:\users\Seungjae\.vscode\extensions\devCAT.lua-debug-0.1.1
+copy /y *.dll c:\users\Seungjae\.vscode\extensions\devCAT.lua-debug-1.0.1
+copy /y *.exe c:\users\Seungjae\.vscode\extensions\devCAT.lua-debug-1.0.1
