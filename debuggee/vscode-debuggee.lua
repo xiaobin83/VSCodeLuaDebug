cffc3734adbe68feada8de9219d7cc428528f545
xiaobin83 <xiaobin.huang@gmail.com>
2017-01-05 22:47:30 +0800

    setfenv/getfenv replacement

--- debuggee/vscode-debuggee.lua
+++ debuggee/vscode-debuggee.lua
@@ -18,6 +18,26 @@ local function defaultOnError(e)
 	print('****************************************************')
 end
 
+
+if not setfenv then -- Lua 5.2+
+  -- based on http://lua-users.org/lists/lua-l/2010-06/msg00314.html
+  -- this assumes f is a function
+  local function findenv(f)
+    local level = 1
+    repeat
+      local name, value = debug.getupvalue(f, level)
+      if name == '_ENV' then return level, value end
+      level = level + 1
+    until name == nil
+    return nil end
+  getfenv = function (f) return(select(2, findenv(f)) or _G) end
+  setfenv = function (f, t)
+    local level = findenv(f)
+    if level then debug.setupvalue(f, level, t) end
+    return f end
+end
+
+
 -------------------------------------------------------------------------------
 local sethook = debug.sethook
 debug.sethook = nil
