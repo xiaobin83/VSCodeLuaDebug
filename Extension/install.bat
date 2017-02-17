f5f5999814d298637ca9457940600a3ccb1612bc
xiaobin83 <xiaobin.huang@gmail.com>
2017-02-17 13:48:30 +0800

    find extension path by env var

--- Extension/install.bat
+++ Extension/install.bat
@@ -1,2 +1,4 @@
-copy /y *.dll c:\users\Seungjae\.vscode\extensions\devCAT.lua-debug-1.0.1
-copy /y *.exe c:\users\Seungjae\.vscode\extensions\devCAT.lua-debug-1.0.1
+set VERSION=1.0.8
+set INSTALL_PATH=%HOMEDRIVE%%HOMEPATH%\.vscode\extensions\devCAT.lua-debug-%VERSION%
+copy /y *.dll %INSTALL_PATH%
+copy /y *.exe %INSTALL_PATH%
